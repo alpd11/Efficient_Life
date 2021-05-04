8908bb8dcdb8203547450f98f2fd9f28d7aace0f
Tulsonic <tulgar.dinc@gmail.com>
2021-05-04 10:37:57 +0300

    initial commit

--- lib/activity_history.dart
+++ lib/activity_history.dart
@@ -3,6 +3,20 @@ import 'package:flutter/material.dart';
 class ActivityHistory extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return Row(
+      children: [
+        HistoryCard(),
+        HistoryCard(),
+        HistoryCard(),
+        HistoryCard(),
+      ],
+    );
+  }
+}
+
+class HistoryCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
      return Text('Task array');
   }
 }
